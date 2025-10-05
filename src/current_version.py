772bdb0dd9ef0182483552267b6ea75400ec9276
Reim, Elijah <elijah.reim@wartsila.com>
2025-10-05 18:41:35 +0200

    Fixed current_version.py generation

--- src/current_version.py
+++ src/current_version.py
@@ -1,6 +1,1 @@
-"""
-You may auto-generate the file if needed by your workflow.
-The only thing it should do is to contain the VERSION variable.
-"""
-
-VERSION = "Private build"
+sdfsadVERSION = ""
