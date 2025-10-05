c9942ee1aa7a9a1935d91e49d2eb8dafe7e9d1a4
Reim, Elijah <elijah.reim@wartsila.com>
2025-10-05 15:04:16 +0200

    The base version

---
+++ build-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source ./_build_stages.sh
+
+
+echo "Building ${PROGRAM_VERSION}"
+
+create_venv
+set_version
+activate_venv
+install_requirements
+prepare_gettext
+build_msgs
+run_tests
+run_mypy
+run_linter
+pyinstaller_build
