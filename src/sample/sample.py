0631f160185ad523c63baa25594312facbc4c122
Reim, Elijah <elijah.reim@wartsila.com>
2025-10-05 15:00:13 +0200

    No GitHub Actions yet, but the base version is here

---
+++ src/sample/sample.py
@@ -0,0 +1,9 @@
+# REGISTER_DOCTEST
+
+
+def sample_program ():
+    """
+    >>> sample_program()
+    0
+    """
+    return 0
---
+++ src/tests/sample_program/test_sample_program.py
@@ -0,0 +1,10 @@
+"""
+the unit test file name should always start with "test_"!
+"""
+import unittest
+
+from sample.sample import sample_program
+
+class SampleProgramTestCase(unittest.TestCase):
+    def test_sample_program(self):
+        self.assertEqual(sample_program(), 0)
