be087ecc8419e4178cfd2be95406f184aa2c9f8b
Reim, Elijah <elijah.reim@wartsila.com>
2025-10-05 15:04:16 +0200

    The base version

---
+++ _build_stages.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+PRJ_NAME = "python-prj-template"  # Change
+
+check_res_and_popd_on_exit () {
+  if [[ $? -ne 0 ]]; then
+    echo "Something has utterly failed!"
+    popd
+    exit $?
+  fi
+}
+
+create_venv () {
+  echo "Creating venv"
+  pushd `pwd`
+  python -m venv `pwd`/venv
+  check_res_and_popd_on_exit
+}
+
+set_version () {
+  if [ "${PROGRAM_VERSION}" != "" ]; then
+    echo "VERSION = \"${PROGRAM_VERSION}\"" >| puml_compiler/src/version.txt
+    cat puml_compiler/src/version.txt
+  fi
+}
+
+activate_venv () {
+  if [ "$OSTYPE" == "linux-gnu" ]; then
+    echo "Linux venv activation"
+    source venv/bin/activate
+    PYSCRIPTS=`pwd`/venv/bin
+  else
+    echo "Windows venv activation"
+
+    # Check if activator.py is missing in Scripts
+    if [ ! -f venv/Scripts/activator.py ]; then
+      # Try to copy from Scripts/nt if it exists
+      if [ -f venv/Scripts/nt/activator.py ]; then
+        cp venv/Scripts/nt/activator.py venv/Scripts/
+      else
+        # Fallback: copy your custom activator
+        cp utils/activator.py venv/Scripts/activator.py
+      fi
+    fi
+
+    venv/Scripts/python venv/Scripts/activator.py
+    PYSCRIPTS=`pwd`/venv/Scripts
+  fi
+}
+
+install_requirements () {
+  pushd .
+  echo "Installing requirements"
+  ${PYSCRIPTS}/pip install -r requirements.txt
+  check_res_and_popd_on_exit
+}
+
+prepare_gettext() {
+  pushd venv
+  if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
+    echo "Preparing gettext tools for Windows..."
+
+    if [[ ! -f gettext.zip ]]; then
+      echo "Downloading gettext tools..."
+      curl -L -o gettext.zip https://github.com/vslavik/gettext-tools-windows/releases/download/v0.26/gettext-tools-windows-0.26.zip
+      check_res_and_popd_on_exit
+    else
+      echo "gettext.zip already exists, skipping download."
+    fi
+
+    echo "Extracting gettext tools..."
+    unzip -o -q gettext.zip -d gettext-bin
+    check_res_and_popd_on_exit
+
+    export PATH="$(pwd)/gettext-bin/bin:$PATH"
+    echo "Gettext tools added to PATH"
+  fi
+  popd
+}
+
+build_msgs () {
+  echo "Building message files"
+  check_res_and_popd_on_exit
+  pushd src
+  xgettext -n -o locales/messages.pot main.py sample/sample.py && \
+  msgmerge -N -U --backup=t locales/en_US/LC_MESSAGES/messages.po locales/messages.pot >locales/tmp_messages.pot && \
+  msgfmt -o locales/en_US/LC_MESSAGES/messages.mo locales/en_US/LC_MESSAGES/messages.po && \
+  msgmerge -N -U --backup=t locales/ru_RU/LC_MESSAGES/messages.po locales/messages.pot >locales/tmp_messages.pot && \
+  msgfmt -o locales/ru_RU/LC_MESSAGES/messages.mo locales/ru_RU/LC_MESSAGES/messages.po
+  check_res_and_popd_on_exit
+  popd
+}
+
+run_tests () {
+  echo "Running tests"
+  pushd src/tests
+  ${PYSCRIPTS}/python run_tests.py
+  check_res_and_popd_on_exit
+  popd
+}
+
+run_mypy () {
+  echo "Running Mypy"
+  pushd src
+  ${PYSCRIPTS}/mypy .
+  check_res_and_popd_on_exit
+  popd
+}
+
+run_black() {
+  echo "Reformatting the code with black"
+  pushd src
+  ${PYSCRIPTS}/black .
+  check_res_and_popd_on_exit
+  popd
+}
+
+run_linter() {
+  echo "Linting with flake8"
+  pushd src
+  ${PYSCRIPTS}/flake8 .
+  check_res_and_popd_on_exit
+  popd
+}
+
+pyinstaller_build () {
+  echo "Building a PyInstaller executable"
+  S=':'
+  if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
+       S=';'
+  fi
+  mkdir out
+  pushd src
+  rm -rf ../out/distr/${PRJ_NAME}
+  rm -rf ../out/temp
+  ${PYSCRIPTS}/pyinstaller -F --add-data ./templates/template.xml${S}templates --add-data ./locales/en_US/LC_MESSAGES/messages.mo${S}locales/en_US/LC_MESSAGES --add-data ./locales/ru_RU/LC_MESSAGES/messages.mo${S}locales/ru_RU/LC_MESSAGES --noconfirm --distpath ../out/distr/${PRJ_NAME} -p . --workpath ../out/temp --clean ./main.py
+  check_res_and_popd_on_exit
+  popd
+}
---
+++ src/settings.py
@@ -0,0 +1,26 @@
+"""
+Import this file ONLY after setting the LANGUAGE variable in the main module!
+You should assign a locale name to it ("en_US", "ru_RU", etc.)
+If you initialise the locale somewhere else, still do it before the first
+import of settings and use something like:
+setattr(sys.modules["__main__"], "LANGUAGE", "en_US")
+"""
+import os.path
+import gettext
+import sys
+
+SRC_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+SRC_DIR = getattr(sys, "_MEIPASS", SRC_DIR)  # PyInstaller support
+
+LOCALES_DIR = os.path.join(SRC_DIR, "locales")
+TEMPLATE_DIR = os.path.join(SRC_DIR, "templates")
+
+if SRC_DIR not in sys.path:  # pragma: no cover
+    sys.path.append(SRC_DIR)
+
+LANGUAGE = getattr(sys.modules["__main__"], "LANGUAGE", "en_US")
+
+TRANSLATION = gettext.translation(
+    "messages", localedir=LOCALES_DIR, languages=[LANGUAGE]
+)
+_ = TRANSLATION.gettext
